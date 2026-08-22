7b0f35934ffe4d42172f93196230d92d53424148
AdhyayanJadhav <adhyayanjadhav40@gmail.com>
2026-08-22 07:51:45 +0530

    added a new program intermediate calculator

---
+++ intermediate calculator.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    float num1,num2;
+    char sign;
+    printf("enter first number: ");
+    scanf("%f", &num1);
+    printf("enter a mathematical opperation: ");
+    scanf(" %c",&sign);
+    printf("enter second number: ");
+    scanf("%f",&num2);
+
+    switch(sign)
+        {
+            case '+':
+                printf("%.2f + %.2f = %.2f",num1,num2,num1+num2);
+                break;
+            case '-':
+                printf("%.2f - %.2f = %.2f",num1,num2,num1-num2);
+                break;
+            case '*':
+                printf("%.2f * %.2f = %.2f",num1,num2,num1*num2);
+                break;
+            case '/':
+                {if (num2 == 0)
+                    printf("ERROR... cannot be divided by zero");
+                else
+                    printf("%.2f / %.2f = %.2f",num1,num2,num1/num2);
+                }
+                break;
+            default:
+                printf("ERROR... check your opperation again !!!");
+                break;
+        }
+    return 0;
+}
